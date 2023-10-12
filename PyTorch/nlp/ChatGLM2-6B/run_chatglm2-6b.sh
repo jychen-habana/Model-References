3cf61f9fe2ba58748ab1029ffdeda60955601fa0
Jinyan Chen <jychen@habana.ai>
2023-10-12 07:52:44 -0700

    [ChatGLM2-6B] support multi prompt bs perf measure

--- PyTorch/nlp/ChatGLM2-6B/run_chatglm2-6b.sh
+++ PyTorch/nlp/ChatGLM2-6B/run_chatglm2-6b.sh
@@ -3,7 +3,6 @@ set -ex
 
 BIN=`dirname ${0}`
 BIN=`cd ${BIN}; pwd`
-WORK_SPACE=`cd ${BIN};cd ../../; pwd`
 
 # MAX_LENGTH="512 1024 2048"
 # INPUT_LENGTH="32 128 256 512 1024 1536 1792 1920"
@@ -13,13 +12,11 @@ LLM_BS="1 2 4"
 INPUT_LENGTH="32 128"
 MAX_LENGTH="512 1024"
 
-export TRANSFORMERS_CACHE=${RUN_DATA_DST}/hub
 if [ "${OFFLINE_MODE}" == true ]
 then
     export TRANSFORMERS_OFFLINE=1
 else
     export -n TRANSFORMERS_OFFLINE
-    export -n TRANSFORMERS_CACHE
 fi
 
 TMP_DIR=${BIN}/tmp
@@ -42,9 +39,9 @@ python -m pip install -r ${CHATGLM2_6B_DIR}/requirements.txt
 CMD="python ${CHATGLM2_6B_DIR}/cli_demo.py"
 if [ "${OFFLINE_MODE}" == false ]
 then
-    ${CMD}
+    ${CMD} || echo "Finished weights downloading!"
 fi
-cp -rf ${CHATGLM2_6B_DIR}/models--THUDM--chatglm2-6b/* ${TRANSFORMERS_CACHE}/models--THUDM--chatglm2-6b/snapshots/*/
+cp -rf ${CHATGLM2_6B_DIR}/models--THUDM--chatglm2-6b/* ${HOME}/.cache/huggingface/hub/models--THUDM--chatglm2-6b/snapshots/*/
 rm -rf ${HOME}/.cache/huggingface/modules/transformers_modules/THUDM/chatglm2-6b
 
 for l in ${MAX_LENGTH}
@@ -55,11 +52,11 @@ do
         do
             for r in ${RUN_REPEAT}
             do
-                ${CMD} --max_length ${l} --prompt ${i} --batch_size ${b} --repeat ${r} 2>&1 | tee chatglm2-6b_${l}_${i}_${b}_${r}.log
+                ${CMD} --max_length ${l} --prompt ${i} --batch_size ${b} --repeat ${r} 2>&1 | tee ${HOME}/chatglm2-6b_${l}_${i}_${b}_${r}.log
             done
         done
     done
 done
 
-# rm -rf ${TMP_DIR}
+rm -rf ${TMP_DIR}
 
